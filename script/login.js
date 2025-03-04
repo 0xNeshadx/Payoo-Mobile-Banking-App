41522c0b163a63e4d2a74be7281dcfcc8b84958c
0xNeshadx <neshadimagify@gmail.com>
2025-03-04 22:56:27 +0600

    added some code

--- script/login.js
+++ script/login.js
@@ -1,3 +1,19 @@
-document.getElementById("login-btn").addEventListener("click", function(){
-    
+document.getElementById("login-btn").addEventListener("click", function(event){
+    event.preventDefault();
+    const accountNumber = document.getElementById("Account-number").value;
+    const pin = document.getElementById("pin").value;
+    const convertedPin = parseInt(pin)
+    if (accountNumber.length === 11) {
+
+        if (convertedPin === 1234) {
+          window.location.href="main.html"
+        } 
+        else {
+         alert("No")
+        }
+    }
+    else {
+        alert("Press valid account")
+    }
+
 })
