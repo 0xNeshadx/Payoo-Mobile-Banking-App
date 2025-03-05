5581083d59b4cdc7f28813db0df9063bfac012e9
0xNeshadx <neshadimagify@gmail.com>
2025-03-05 11:13:51 +0600

    added some code

--- script/addmoney.js
+++ script/addmoney.js
@@ -7,12 +7,17 @@ document.getElementById("add-money").addEventListener("click", function(event) {
     const mainBalance = document.getElementById("main-balance").innerText;
     const convertedMainbalance = parseInt(mainBalance)
 
-    if (convertedPin === 1234) {
-        const sum = convertedMainbalance + convertedAmount;
-        document.getElementById("main-balance").innerText =  sum;
+    if (amount && pin) {
+        if (convertedPin === 1234) {
+            const sum = convertedMainbalance + convertedAmount;
+            document.getElementById("main-balance").innerText =  sum;
+        }
+        else {
+           alert("Enter valid pin")
+    
+        }
     }
     else {
-        console.log("pin valid")
-
+        alert("Enter amount")
     }
 })
